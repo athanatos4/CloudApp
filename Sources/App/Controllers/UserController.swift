aae6ec299c67ebc80734ec7c0110a741ae037c10
Arnaud <arnaud.vallot@student.ulg.ac.be>
2018-08-13 14:56:26 +0200

    corrected an error with login, and switched to SHA256 for hashing

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -6,7 +6,8 @@ import Crypto
 class UserController {
     func create(_ req: Request) throws -> Future<User.PublicUser> {
         return try req.content.decode(User.self).flatMap() { user in
-            user.password = try req.make(BCryptDigest.self).hash(user.password)
+            print("in create: " + user.password)
+            user.password = try SHA256.hash(user.password).hexEncodedString()
             user.id = nil
             return user.save(on: req).flatMap(to: User.PublicUser.self) { createdUser in
                 let accessToken = try Token.createToken(forUser: createdUser)
@@ -21,9 +22,11 @@ class UserController {
     func login(_ req: Request) throws -> Future<User.PublicUser> {
         let username: String = try req.content.syncGet(at: "username")
         let password: String = try req.content.syncGet(at: "password")
-        let hashed = try req.make(BCryptDigest.self).hash(password)
-        return User.query(on: req).filter(\User.username == username).filter(\User.password == hashed).first()
+        let hashed = try SHA256.hash(password).hexEncodedString()
+        return User.query(on: req).filter(\User.username == username)
+        .filter(\User.password == hashed).first()
         .flatMap() { usr throws -> Future<Token> in
+            print("user: " + usr!.password)
             let randomString = Token.randomToken(withLength: 60)
             let t = try Token(token: randomString, userId: usr!.requireID())
             return t.save(on: req)
